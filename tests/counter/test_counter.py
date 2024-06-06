a2f840112fc687a3d80f1db017fae4157305a027
eduzissimo <eduardo.alves14@hotmail.com>
2024-06-06 18:04:46 -0300

    Feat: Req 1

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,12 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    raise
+    count_path = count_ocurrences("data/jobs.csv", 'Python')
+    assert count_path == 1639
+
+    count_path = count_ocurrences("data/jobs.csv", 'C#')
+    assert count_path == 57
+
+    count_path = count_ocurrences("data/jobs.csv", 'Javascript')
+    assert count_path == 122
