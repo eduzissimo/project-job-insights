73817edb5081842a7b2cbaeed654520a104885f1
eduzissimo <eduardo.alves14@hotmail.com>
2024-06-06 18:51:47 -0300

    Feat: 6 e 7 - max e min salary

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -7,10 +7,28 @@ class ProcessSalaries(ProcessJobs):
         super().__init__()
 
     def get_max_salary(self) -> int:
-        pass
+        max_salary = None
+        for job in self.jobs_list:
+            if job.get('max_salary') and job['max_salary'].isdigit():
+                salary = int(job['max_salary'])
+                max_salary = (
+                    max(max_salary, salary) 
+                    if max_salary is not None 
+                    else salary
+                )
+        return max_salary
 
     def get_min_salary(self) -> int:
-        pass
+        min_salary = None
+        for job in self.jobs_list:
+            if job.get('min_salary') and job['min_salary'].isdigit():
+                salary = int(job['min_salary'])
+                min_salary = (
+                    min(min_salary, salary) 
+                    if min_salary is not None 
+                    else salary
+                )
+        return min_salary
 
     def matches_salary_range(self, job: Dict, salary: Union[int, str]) -> bool:
         pass
